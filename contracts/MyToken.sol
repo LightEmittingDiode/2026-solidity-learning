4faa33ef579fc77c136d0ecd2d2361008af0b57e
LightEmittingDiode <harryfriend0000@gmail.com>
2026-06-26 23:24:53 +0900

    mint function

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,30 +1,25 @@
-// SPDX-License-Indentifier: MIT
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
 contract Mytoken {
     string public name;
     string public symbol;
-    uint8 public decimals; // 1 wei --> 1 * 10^-18
+    uint8 public decimals;
 
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
 
+    address public owner; // (이 변수를 굳이 안 써도 작동합니다)
+
     constructor(string memory _name, string memory _symbol, uint8 _decimal) {
-        name = _name;
+        name = _name; 
         symbol = _symbol;
         decimals = _decimal;
+        _mint(1 * 10 ** uint256(decimals), msg.sender);
     }
 
-    // function totalSupply() external view returns (uint256 totalSupply) {
-    //     return totalSupply;
-    // }
-
-    // function balanceOf(address owner) external view returns (uint256) {
-    //     return balanceOf[owner];
-    // }
-
-    // function name() external view returns (string memory) {
-    //     return name;
-    // }
+    function _mint(uint256 amount, address to) internal {
+        totalSupply += amount;
+        balanceOf[to] += amount;
+    }
 }
