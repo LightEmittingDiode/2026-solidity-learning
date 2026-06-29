c3fe1306f517bc6731ad43ee4446c3800c8def57
LightEmittingDiode <harryfriend0000@gmail.com>
2026-06-29 22:41:06 +0900

    Added Approve and TranferFrom

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,25 +1,57 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-contract Mytoken {
+contract MyToken {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed spender, uint256 amount);
+
     string public name;
     string public symbol;
-    uint8 public decimals;
+    uint8 public decimals = 18;
 
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
+    mapping (address => mapping(address => uint256)) allowance;
 
-    address public owner; // (이 변수를 굳이 안 써도 작동합니다)
-
-    constructor(string memory _name, string memory _symbol, uint8 _decimal) {
-        name = _name; 
+    constructor(
+        string memory _name, 
+        string memory _symbol, 
+        uint8 _decimal, 
+        uint256 _amount
+    ) {
+        name = _name;
         symbol = _symbol;
         decimals = _decimal;
-        _mint(1 * 10 ** uint256(decimals), msg.sender);
+        _mint(msg.sender, _amount * 10 ** uint256(decimals));
     }
 
-    function _mint(uint256 amount, address to) internal {
-        totalSupply += amount;
+    function approve(address spender, uint256 amount) external {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(spender, amount);
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external {
+        address spender = msg.sender;
+        require(allowance[from][spender] >= amount, "insufficient allowance");
+        allowance[from][spender] -= amount;
+        balanceOf[from] -= amount;
         balanceOf[to] += amount;
+        emit Transfer(from, to, amount);
+    }
+
+    function _mint(address owner, uint256 amount) internal {
+        totalSupply += amount;
+        balanceOf[owner] += amount;
+
+        emit Transfer(address(0), owner, amount);
+    }
+
+    function transfer(address to, uint256 amount) external {
+        require(balanceOf[msg.sender] >= amount, "insufficient balance");
+
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+
+        emit Transfer(msg.sender, to, amount);
     }
 }
